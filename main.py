975f5cbf8f5387d88f55779e7bba563e48fec862
Pixelbo <pixelbo21@gmail.com>
2021-09-12 19:53:23 +0200

    merde

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
 import numpy as np
 import cv2
 
-
-def nothing(x):
-    pass
-
-
-### Create a black image, a window
-cv2.namedWindow('image')
-
-# create trackbars for color change
-cv2.createTrackbar('min', 'image', 200, 500, nothing)
-cv2.createTrackbar('max', 'image', 200, 500, nothing)
-
 cap = cv2.VideoCapture(1)
 
 while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    blur = cv2.GaussianBlur(frame, (5, 5), 0)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(gray, (0,0,0), (0,0,70))
+    blur2 = cv2.GaussianBlur(mask, (5, 5), 0)
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    edges = cv2.Canny(blur, cv2.getTrackbarPos('min', 'image'), cv2.getTrackbarPos('max', 'image'))
+    edges = cv2.Canny(blur2, 100,200)
 
     ret, thresh_img = cv2.threshold(edges, 91, 255, cv2.THRESH_BINARY)
     contours = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
     for c in contours:
-        rect = cv2.minAreaRect(c)
-        if rect[1][1] != 0:
-            ratio = rect[1][0]/rect[1][1]
-            area = rect[1][0]*rect[1][1]
-            if area > 200 and ratio<0.7:
-                box = cv2.boxPoints(rect)
-                box = np.int0(box)
-                im = cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
-                cv2.putText(frame, str(round(ratio,2)),(int(rect[0][0]),int(rect[0][1])), cv2.FONT_HERSHEY_SIMPLEX, 1 , (255,0,0))
+        x, y, w, h = cv2.boundingRect(c)
+        ratio = w / h
+        area = w*h
+        if area > 100 and area < 2000 and ratio<=0.7 and ratio >=0.4:
+            img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, str(round(ratio,2)),(x,y), cv2.FONT_HERSHEY_SIMPLEX, 1 , (255,0,0))
+            crop = img[y:y + h, x:x + w]
 
-    cv2.imshow('edges', thresh_img)
+
+    cv2.imshow('mask', mask)
     cv2.imshow('frame', frame)
+    cv2.imshow('edges', edges)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
